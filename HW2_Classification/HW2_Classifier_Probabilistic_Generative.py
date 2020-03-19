55f02e92fa010147ef252c20465fce2d10e00e9d
IPINGCHOU <f77252426@gmail.com>
2020-03-19 11:25:07 +0000

    tutorial probabilistic generative

---
+++ HW2_Classification/HW2_Classifier_Probabilistic_Generative.py
@@ -0,0 +1,142 @@
+#%%
+import numpy as np
+
+np.random.seed(0)
+X_train_fpath = './data/X_train'
+Y_train_fpath = './data/Y_train'
+X_test_fpath = './data/X_test'
+output_fpath = './output_{}.csv'
+
+# Parse csv files to numpy array
+with open(X_train_fpath) as f:
+    next(f)
+    X_train = np.array([line.strip('\n').split(',')[1:] for line in f], dtype = float)
+with open(Y_train_fpath) as f:
+    next(f)
+    Y_train = np.array([line.strip('\n').split(',')[1] for line in f], dtype = float)
+with open(X_test_fpath) as f:
+    next(f)
+    X_test = np.array([line.strip('\n').split(',')[1:] for line in f], dtype = float)
+
+# Normalize training and testing data
+
+def _normalize(X, train = True, specified_column = None, X_mean = None, X_std = None):
+    # This function normalizes specific columns of X.
+    # The mean and standard variance of training data will be reused when processing testing data.
+    #
+    # Arguments:
+    #     X: data to be processed
+    #     train: 'True' when processing training data, 'False' for testing data
+    #     specific_column: indexes of the columns that will be normalized. If 'None', all columns
+    #         will be normalized.
+    #     X_mean: mean value of training data, used when train = 'False'
+    #     X_std: standard deviation of training data, used when train = 'False'
+    # Outputs:
+    #     X: normalized data
+    #     X_mean: computed mean value of training data
+    #     X_std: computed standard deviation of training data
+
+    if specified_column == None:
+        specified_column = np.arange(X.shape[1])
+    if train:
+        X_mean = np.mean(X[:, specified_column] ,0).reshape(1, -1)
+        X_std  = np.std(X[:, specified_column], 0).reshape(1, -1)
+
+    X[:,specified_column] = (X[:, specified_column] - X_mean) / (X_std + 1e-8)
+     
+    return X, X_mean, X_std
+
+
+
+X_train, X_mean, X_std = _normalize(X_train, train = True)
+X_test, _, _= _normalize(X_test, train = False, specified_column = None, X_mean = X_mean, X_std = X_std)
+
+train_size = X_train.shape[0]
+test_size = X_test.shape[0]
+data_dim = X_train.shape[1]
+print('Size of training set: {}'.format(train_size))
+print('Size of testing set: {}'.format(test_size))
+print('Dimension of data: {}'.format(data_dim))
+
+#%%
+def _shuffle(X, Y):
+    # This function shuffles two equal-length list/array, X and Y, together.
+    randomize = np.arange(len(X))
+    np.random.shuffle(randomize)
+    return (X[randomize], Y[randomize])
+
+def _sigmoid(z):
+    # Sigmoid function can be used to calculate probability.
+    # To avoid overflow, minimum/maximum output value is set.
+    return np.clip(1 / (1.0 + np.exp(-z)), 1e-8, 1 - (1e-8))
+
+def _f(X, w, b):
+    # This is the logistic regression function, parameterized by w and b
+    #
+    # Arguements:
+    #     X: input data, shape = [batch_size, data_dimension]
+    #     w: weight vector, shape = [data_dimension, ]
+    #     b: bias, scalar
+    # Output:
+    #     predicted probability of each row of X being positively labeled, shape = [batch_size, ]
+    return _sigmoid(np.matmul(X, w) + b)
+
+def _predict(X, w, b):
+    # This function returns a truth value prediction for each row of X 
+    # by rounding the result of logistic regression function.
+    return np.round(_f(X, w, b)).astype(np.int)
+    
+def _accuracy(Y_pred, Y_label):
+    # This function calculates prediction accuracy
+    acc = 1 - np.mean(np.abs(Y_pred - Y_label))
+    return acc
+# Compute in-class mean
+X_train_0 = np.array([x for x, y in zip(X_train, Y_train) if y == 0])
+X_train_1 = np.array([x for x, y in zip(X_train, Y_train) if y == 1])
+
+mean_0 = np.mean(X_train_0, axis = 0)
+mean_1 = np.mean(X_train_1, axis = 0)  
+
+# Compute in-class covariance
+cov_0 = np.zeros((data_dim, data_dim))
+cov_1 = np.zeros((data_dim, data_dim))
+
+for x in X_train_0:
+    cov_0 += np.dot(np.transpose([x - mean_0]), [x - mean_0]) / X_train_0.shape[0]
+for x in X_train_1:
+    cov_1 += np.dot(np.transpose([x - mean_1]), [x - mean_1]) / X_train_1.shape[0]
+
+# Shared covariance is taken as a weighted average of individual in-class covariance.
+cov = (cov_0 * X_train_0.shape[0] + cov_1 * X_train_1.shape[0]) / (X_train_0.shape[0] + X_train_1.shape[0])
+
+#%%
+# Compute inverse of covariance matrix.
+# Since covariance matrix may be nearly singular, np.linalg.inv() may give a large numerical error.
+# Via SVD decomposition, one can get matrix inverse efficiently and accurately.
+u, s, v = np.linalg.svd(cov, full_matrices=False)
+inv = np.matmul(v.T * 1 / s, u.T)
+
+# Directly compute weights and bias
+w = np.dot(inv, mean_0 - mean_1)
+b =  (-0.5) * np.dot(mean_0, np.dot(inv, mean_0)) + 0.5 * np.dot(mean_1, np.dot(inv, mean_1))\
+    + np.log(float(X_train_0.shape[0]) / X_train_1.shape[0]) 
+
+# Compute accuracy on training set
+Y_train_pred = 1 - _predict(X_train, w, b)
+print('Training accuracy: {}'.format(_accuracy(Y_train_pred, Y_train)))
+#%%
+# Predict testing labels
+predictions = 1 - _predict(X_test, w, b)
+with open(output_fpath.format('generative'), 'w') as f:
+    f.write('id,label\n')
+    for i, label in  enumerate(predictions):
+        f.write('{},{}\n'.format(i, label))
+
+# Print out the most significant weights
+ind = np.argsort(np.abs(w))[::-1]
+with open(X_test_fpath) as f:
+    content = f.readline().strip('\n').split(',')
+features = np.array(content)
+for i in ind[0:10]:
+    print(features[i], w[i])
+#%%
