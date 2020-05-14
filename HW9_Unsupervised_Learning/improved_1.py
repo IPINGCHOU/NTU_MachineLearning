30751398a7520b84c8b9de2fcb4e3ef7d4a8b4c2
IPINGCHOU <f77252426@gmail.com>
2020-05-14 06:37:46 +0000

    deeper tutorial model trail

---
+++ HW9_Unsupervised_Learning/improved_1.py
@@ -0,0 +1,341 @@
+import numpy as np
+
+def preprocess(image_list):
+    """ Normalize Image and Permute (N,H,W,C) to (N,C,H,W)
+    Args:
+      image_list: List of images (9000, 32, 32, 3)
+    Returns:
+      image_list: List of images (9000, 3, 32, 32)
+    """
+    image_list = np.array(image_list)
+    image_list = np.transpose(image_list, (0, 3, 1, 2))
+    image_list = (image_list / 255.0) * 2 - 1
+    image_list = image_list.astype(np.float32)
+    return image_list
+
+from torch.utils.data import Dataset
+
+class Image_Dataset(Dataset):
+    def __init__(self, image_list):
+        self.image_list = image_list
+    def __len__(self):
+        return len(self.image_list)
+    def __getitem__(self, idx):
+        images = self.image_list[idx]
+        return images
+#%%
+from torch.utils.data import DataLoader
+import os
+data_folder = './data'
+trainX_path = os.path.join(data_folder, 'trainX.npy')
+
+trainX = np.load(trainX_path)
+trainX_preprocessed = preprocess(trainX)
+img_dataset = Image_Dataset(trainX_preprocessed)
+
+#%%
+import random
+import torch
+
+def count_parameters(model, only_trainable=False):
+    if only_trainable:
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    else:
+        return sum(p.numel() for p in model.parameters())
+
+def same_seeds(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    np.random.seed(seed)  # Numpy module.
+    random.seed(seed)  # Python random module.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+#%%
+# baseline autoencoder
+# same architecture as Hinton
+import torch.nn as nn
+class AE(nn.Module):
+    def __init__(self):
+        super(AE, self).__init__()
+        
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 128, 3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2),
+            nn.Conv2d(128, 256, 3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(256),
+            nn.MaxPool2d(2),
+            nn.Conv2d(256, 512, 3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(512),
+            nn.MaxPool2d(2),
+            nn.Conv2d(512, 1024, 3, stride=1, padding=1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(1024),
+            nn.MaxPool2d(2), # 1024 * 2 * 2
+        )
+
+ 
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(1024, 512, 5, stride=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(512, 256, 7, stride=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 128, 10, stride=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 3, 12, stride=1),
+            # nn.ReLU(True),
+            # nn.ConvTranspose2d(128, 64, 8, stride=1),
+            # nn.ReLU(True),
+            # nn.ConvTranspose2d(64, 3, 7, stride=1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x1 = self.encoder(x)
+        x  = self.decoder(x1)
+        return x1, x
+
+#%%
+# training 
+import torch
+from torch import optim
+
+same_seeds(0)
+
+model = AE().cuda()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-5, weight_decay=1e-5)
+
+model.train()
+n_epoch = 200
+
+# 準備 dataloader, model, loss criterion 和 optimizer
+img_dataloader = DataLoader(img_dataset, batch_size=1024, shuffle=True)
+
+# === h para ===
+MODEL_SAVEFOLDER = './improve2_model'
+
+#%%
+# 主要的訓練過程
+for epoch in range(n_epoch):
+    for data in img_dataloader:
+        img = data
+        img = img.cuda()
+
+        output1, output = model(img)
+        loss = criterion(output, img)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if (epoch+1) % 10 == 0:
+            torch.save(model.state_dict(), MODEL_SAVEFOLDER + '/checkpoint_{}.pth'.format(epoch+1))
+            
+    print('epoch [{}/{}], loss:{:.5f}'.format(epoch+1, n_epoch, loss.data))
+
+# 訓練完成後儲存 model
+torch.save(model.state_dict(), MODEL_SAVEFOLDER+'/last_checkpoint.pth')
+
+#%%
+import numpy as np
+
+def cal_acc(gt, pred):
+    """ Computes categorization accuracy of our task.
+    Args:
+      gt: Ground truth labels (9000, )
+      pred: Predicted labels (9000, )
+    Returns:
+      acc: Accuracy (0~1 scalar)
+    """
+    # Calculate Correct predictions
+    correct = np.sum(gt == pred)
+    acc = correct / gt.shape[0]
+    # 因為是 binary unsupervised clustering，因此取 max(acc, 1-acc)
+    return max(acc, 1-acc)
+
+import matplotlib.pyplot as plt
+
+def plot_scatter(feat, label, dot_size = 1,savefig=None):
+    """ Plot Scatter Image.
+    Args:
+      feat: the (x, y) coordinate of clustering result, shape: (9000, 2)
+      label: ground truth label of image (0/1), shape: (9000,)
+    Returns:
+      None
+    """
+    X = feat[:, 0] 
+    Y = feat[:, 1] 
+    plt.scatter(X, Y, c = label, s = dot_size)
+    plt.legend(loc='best')
+    if savefig is not None:
+        plt.savefig(savefig)
+    plt.show()
+    return
+
+import torch
+from sklearn.decomposition import KernelPCA
+from sklearn.manifold import TSNE
+from sklearn.cluster import MiniBatchKMeans, SpectralClustering, KMeans
+
+def inference(X, model, batch_size=256):
+    X = preprocess(X)
+    dataset = Image_Dataset(X)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    latents = []
+    for i, x in enumerate(dataloader):
+        x = torch.FloatTensor(x)
+        vec, img = model(x.cuda())
+        if i == 0:
+            latents = vec.view(img.size()[0], -1).cpu().detach().numpy()
+        else:
+            latents = np.concatenate((latents, vec.view(img.size()[0], -1).cpu().detach().numpy()), axis = 0)
+    print('Latents Shape:', latents.shape)
+    return latents
+#%%
+def predict(latents):
+    # First Dimension Reduction
+    transformer = KernelPCA(n_components=KPCA_comp, kernel='rbf', n_jobs=-1, random_state=SK_SEED)
+    kpca = transformer.fit_transform(latents)
+    print('First Reduction Shape:', kpca.shape)
+
+    # # Second Dimesnion Reduction
+    X_embedded = TSNE(n_components=2, perplexity=TSNE_PERPLEXITY, n_iter = TSNE_ITER, init = 'pca', learning_rate=TSNE_LR, random_state=SK_SEED).fit_transform(kpca)
+    print('Second Reduction Shape:', X_embedded.shape)
+
+    # Clustering
+    # KMeans mini
+    # pred = MiniBatchKMeans(n_clusters=2, random_state=SK_SEED).fit(X_embedded)
+    # KMeans
+    pred = KMeans(n_clusters=2, random_state=SK_SEED).fit(X_embedded)
+    
+    # Spectral Clustering
+    # pred = SpectralClustering(n_clusters=2, random_state=SK_SEED, affinity=SP_AFFINITY).fit(X_embedded)
+
+    pred = [int(i) for i in pred.labels_]
+    pred = np.array(pred)
+    return pred, X_embedded
+
+def invert(pred):
+    return np.abs(1-pred)
+
+def save_prediction(pred, out_csv='prediction.csv'):
+    with open(out_csv, 'w') as f:
+        f.write('id,label\n')
+        for i, p in enumerate(pred):
+            f.write(f'{i},{p}\n')
+    print(f'Save prediction to {out_csv}.')
+
+#%%
+# =====================
+SK_SEED = 0
+TSNE_PERPLEXITY = 100
+TSNE_ITER = 1000
+TSNE_LR = 200
+# SP_AFFINITY = 'nearest_neighbors'
+KPCA_comp = 200
+
+valX_path = os.path.join(data_folder, 'valX.npy')
+valY_path = os.path.join(data_folder, 'valY.npy')
+valX = np.load(valX_path)
+valY = np.load(valY_path)
+
+model.load_state_dict(torch.load(MODEL_SAVEFOLDER+'/last_checkpoint.pth'))
+model.eval()
+latents = inference(valX, model)
+pred_from_latent, emb_from_latent = predict(latents)
+acc_latent = cal_acc(valY, pred_from_latent)
+print('The clustering accuracy is:', acc_latent)
+print('The clustering result:')
+plot_scatter(emb_from_latent, valY,2)
+# plot_scatter(emb_from_latent, valY, savefig='p1_baseline.png')
+
+#%%
+# load model
+
+model = AE().cuda()
+model.load_state_dict(torch.load(MODEL_SAVEFOLDER+'/last_checkpoint.pth'))
+model.eval()
+
+# 準備 data
+trainX = np.load(trainX_path)
+
+# 預測答案
+latents = inference(X=trainX, model=model)
+pred, X_embedded = predict(latents)
+
+plot_scatter(X_embedded, pred)
+#%%
+# 將預測結果存檔，上傳 kaggle
+save_prediction(pred, 'km_deep2_pred.csv')
+
+# 由於是 unsupervised 的二分類問題，我們只在乎有沒有成功將圖片分成兩群
+# 如果上面的檔案上傳 kaggle 後正確率不足 0.5，只要將 label 反過來就行了
+save_prediction(invert(pred), 'km_deep2_pred_invert.csv')
+
+
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 畫出原圖
+plt.figure(figsize=(10,4))
+indexes = [1,2,3,6,7,9]
+imgs = trainX[indexes,]
+for i, img in enumerate(imgs):
+    plt.subplot(2, 6, i+1, xticks=[], yticks=[])
+    plt.imshow(img)
+
+# 畫出 reconstruct 的圖
+inp = torch.Tensor(trainX_preprocessed[indexes,]).cuda()
+latents, recs = model(inp)
+recs = ((recs+1)/2 ).cpu().detach().numpy()
+recs = recs.transpose(0, 2, 3, 1)
+for i, img in enumerate(recs):
+    plt.subplot(2, 6, 6+i+1, xticks=[], yticks=[])
+    plt.imshow(img)
+  
+plt.tight_layout()
+#%%
+
+import glob
+checkpoints_list = sorted(glob.glob(MODEL_SAVEFOLDER+'/checkpoint_*.pth'))
+
+# load data
+dataset = Image_Dataset(trainX_preprocessed)
+dataloader = DataLoader(dataset, batch_size=64, shuffle=False)
+
+points = []
+with torch.no_grad():
+    for i, checkpoint in enumerate(checkpoints_list):
+        print('[{}/{}] {}'.format(i+1, len(checkpoints_list), checkpoint))
+        model.load_state_dict(torch.load(checkpoint))
+        model.eval()
+        err = 0
+        n = 0
+        for x in dataloader:
+            x = x.cuda()
+            _, rec = model(x)
+            err += torch.nn.MSELoss(reduction='sum')(x, rec).item()
+            n += x.flatten().size(0)
+        print('Reconstruction error (MSE):', err/n)
+        latents = inference(X=valX, model=model)
+        pred, X_embedded = predict(latents)
+        acc = cal_acc(valY, pred)
+        print('Accuracy:', acc)
+        points.append((err/n, acc))
+
+import matplotlib.pyplot as plt
+ps = list(zip(*points))
+plt.figure(figsize=(6,6))
+plt.subplot(211, title='Reconstruction error (MSE)').plot(ps[0])
+plt.subplot(212, title='Accuracy (val)').plot(ps[1])
+plt.show()
+
+# %%
