2c2070ca7cb67f8cf929d6029e310bf58571b629
IPINGCHOU <f77252426@gmail.com>
2020-06-04 14:11:02 +0000

    wgan train and generate

---
+++ HW11_GAN/wgan_generate.py
@@ -0,0 +1,135 @@
+#%%
+# reproducing fix seed
+import random
+import torch
+import numpy as np
+
+def same_seeds(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    np.random.seed(seed)  # Numpy module.
+    random.seed(seed)  # Python random module.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+SEED = 1234
+same_seeds(1234)
+
+#%%
+# data set 
+from torch.utils.data import Dataset, DataLoader
+import cv2
+import os
+
+class FaceDataset(Dataset):
+    def __init__(self, fnames, transform):
+        self.transform = transform
+        self.fnames = fnames
+        self.num_samples = len(self.fnames)
+    def __getitem__(self,idx):
+        fname = self.fnames[idx]
+        img = cv2.imread(fname)
+        img = self.BGR2RGB(img) #because "torchvision.utils.save_image" use RGB
+        img = self.transform(img)
+        return img
+
+    def __len__(self):
+        return self.num_samples
+
+    def BGR2RGB(self,img):
+        return cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+import glob
+import torchvision.transforms as transforms
+
+def get_dataset(root):
+    fnames = glob.glob(os.path.join(root, '*'))
+    # resize the image to (64, 64)
+    # linearly map [0, 1] to [-1, 1]
+    transform = transforms.Compose(
+        [transforms.ToPILImage(),
+         transforms.Resize((64, 64)),
+         transforms.ToTensor(),
+         transforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3) ] )
+    dataset = FaceDataset(fnames, transform)
+    return dataset
+
+#%%
+# model design
+import torch.nn as nn
+import torch.nn.functional as F
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+class Generator(nn.Module):
+    """
+    input (N, in_dim)
+    output (N, 3, 64, 64)
+    """
+    def __init__(self, in_dim, dim=64):
+        super(Generator, self).__init__()
+        def dconv_bn_relu(in_dim, out_dim):
+            return nn.Sequential(
+                nn.ConvTranspose2d(in_dim, out_dim, 5, 2,
+                                   padding=2, output_padding=1, bias=False),
+                nn.BatchNorm2d(out_dim),
+                nn.ReLU())
+        self.l1 = nn.Sequential(
+            nn.Linear(in_dim, dim * 8 * 4 * 4, bias=False),
+            nn.BatchNorm1d(dim * 8 * 4 * 4),
+            nn.ReLU())
+        self.l2_5 = nn.Sequential(
+            dconv_bn_relu(dim * 8, dim * 4),
+            dconv_bn_relu(dim * 4, dim * 2),
+            dconv_bn_relu(dim * 2, dim),
+            nn.ConvTranspose2d(dim, 3, 5, 2, padding=2, output_padding=1),
+            nn.Tanh())
+        self.apply(weights_init)
+    def forward(self, x):
+        y = self.l1(x)
+        y = y.view(y.size(0), -1, 4, 4)
+        y = self.l2_5(y)
+        return y
+
+#%%
+# prepare training
+# hyper parameters
+import torch
+from torch import optim
+from torch.autograd import Variable
+import torchvision
+import sys
+
+# hyperparameters 
+batch_size = 128
+z_dim = 100
+lr = 1e-04
+n_epoch = 21
+D_WEIGHT_CLIP = 0.01
+
+checkpoint = sys.argv[1]
+filename = sys.argv[2]
+
+# model
+same_seeds(0)
+z_sample = Variable(torch.randn(100, z_dim)).cuda()
+# model
+G = Generator(in_dim=z_dim).cuda()
+G.load_state_dict(torch.load(checkpoint))
+same_seeds(0)
+#%%
+# start training
+# for logging
+z_sample = Variable(torch.randn(100, z_dim)).cuda()
+G.eval()
+f_imgs_sample = (G(z_sample).data + 1) / 2.0
+torchvision.utils.save_image(f_imgs_sample, filename, nrow=10)
+print(f' | Save some samples to {filename}.')
