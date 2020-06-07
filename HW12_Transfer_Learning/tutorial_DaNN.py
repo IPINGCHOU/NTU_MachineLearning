dfcc1b2df858caaeace24103a1ab7c36d1682eb6
IPINGCHOU <f77252426@gmail.com>
2020-06-07 19:49:00 +0000

    tutorial, DaNN

---
+++ HW12_Transfer_Learning/tutorial_DaNN.py
@@ -0,0 +1,310 @@
+#%%
+# reproducing fix seed
+import random
+import torch
+import numpy as np
+
+def same_seeds(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    np.random.seed(seed)  # Numpy module.
+    random.seed(seed)  # Python random module.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+SEED = 1234
+same_seeds(1234)
+#%%
+# draw real pics
+import matplotlib.pyplot as plt
+
+def no_axis_show(img, title='', cmap=None):
+  # imshow, 縮放模式為nearest。
+  fig = plt.imshow(img, interpolation='nearest', cmap=cmap)
+  # 不要顯示axis。
+  fig.axes.get_xaxis().set_visible(False)
+  fig.axes.get_yaxis().set_visible(False)
+  plt.title(title)
+
+titles = ['horse', 'bed', 'clock', 'apple', 'cat', 'plane', 'television', 'dog', 'dolphin', 'spider']
+plt.figure(figsize=(18, 18))
+for i in range(10):
+  plt.subplot(1, 10, i+1)
+  fig = no_axis_show(plt.imread(f'real_or_drawing/train_data/{i}/{500*i}.bmp'), title=titles[i])
+
+#%%
+# draw graffiti pics
+plt.figure(figsize=(18, 18))
+for i in range(10):
+  plt.subplot(1, 10, i+1)
+  fig = no_axis_show(plt.imread(f'real_or_drawing/test_data/0/' + str(i).rjust(5, '0') + '.bmp'))
+
+#%%
+# Canny Edge Dection
+import cv2
+import matplotlib.pyplot as plt
+titles = ['horse', 'bed', 'clock', 'apple', 'cat', 'plane', 'television', 'dog', 'dolphin', 'spider']
+plt.figure(figsize=(18, 18))
+
+original_img = plt.imread(f'real_or_drawing/train_data/0/0.bmp')
+plt.subplot(1, 5, 1)
+no_axis_show(original_img, title='original')
+
+gray_img = cv2.cvtColor(original_img, cv2.COLOR_RGB2GRAY)
+plt.subplot(1, 5, 2)
+no_axis_show(gray_img, title='gray scale', cmap='gray')
+
+gray_img = cv2.cvtColor(original_img, cv2.COLOR_RGB2GRAY)
+plt.subplot(1, 5, 2)
+no_axis_show(gray_img, title='gray scale', cmap='gray')
+
+canny_50100 = cv2.Canny(gray_img, 50, 100)
+plt.subplot(1, 5, 3)
+no_axis_show(canny_50100, title='Canny(50, 100)', cmap='gray')
+
+canny_150200 = cv2.Canny(gray_img, 150, 200)
+plt.subplot(1, 5, 4)
+no_axis_show(canny_150200, title='Canny(150, 200)', cmap='gray')
+
+canny_250300 = cv2.Canny(gray_img, 250, 300)
+plt.subplot(1, 5, 5)
+no_axis_show(canny_250300, title='Canny(250, 300)', cmap='gray')
+  
+
+# %%
+# data process
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Function
+
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+source_transform = transforms.Compose([
+    # 轉灰階: Canny 不吃 RGB。
+    transforms.Grayscale(),
+    # cv2 不吃 skimage.Image，因此轉成np.array後再做cv2.Canny
+    transforms.Lambda(lambda x: cv2.Canny(np.array(x), 170, 300)),
+    # 重新將np.array 轉回 skimage.Image
+    transforms.ToPILImage(),
+    # 水平翻轉 (Augmentation)
+    transforms.RandomHorizontalFlip(),
+    # 旋轉15度內 (Augmentation)，旋轉後空的地方補0
+    transforms.RandomRotation(15),
+    # 最後轉成Tensor供model使用。
+    transforms.ToTensor(),
+])
+target_transform = transforms.Compose([
+    # 轉灰階: 將輸入3維壓成1維。
+    transforms.Grayscale(),
+    # 縮放: 因為source data是32x32，我們將target data的28x28放大成32x32。
+    transforms.Resize((32, 32)),
+    # 水平翻轉 (Augmentation)
+    transforms.RandomHorizontalFlip(),
+    # 旋轉15度內 (Augmentation)，旋轉後空的地方補0
+    transforms.RandomRotation(15),
+    # 最後轉成Tensor供model使用。
+    transforms.ToTensor(),
+])
+
+source_dataset = ImageFolder('./real_or_drawing/train_data', transform=source_transform)
+target_dataset = ImageFolder('./real_or_drawing/test_data', transform=target_transform)
+
+source_dataloader = DataLoader(source_dataset, batch_size=256, shuffle=True)
+target_dataloader = DataLoader(target_dataset, batch_size=256, shuffle=True)
+test_dataloader = DataLoader(target_dataset, batch_size=128, shuffle=False)
+
+# %%
+# model design
+class FeatureExtractor(nn.Module):
+
+    def __init__(self):
+        super(FeatureExtractor, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(256, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        
+    def forward(self, x):
+        x = self.conv(x).squeeze()
+        return x
+
+class LabelPredictor(nn.Module):
+
+    def __init__(self):
+        super(LabelPredictor, self).__init__()
+
+        self.layer = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU(),
+
+            nn.Linear(512, 512),
+            nn.ReLU(),
+
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, h):
+        c = self.layer(h)
+        return c
+
+class DomainClassifier(nn.Module):
+
+    def __init__(self):
+        super(DomainClassifier, self).__init__()
+
+        self.layer = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+
+            nn.Linear(512, 1),
+        )
+
+    def forward(self, h):
+        y = self.layer(h)
+        return y
+
+#%%
+# preprocessing
+# Adam optimizer for all models
+# CE for class criterion
+# BCE for domain criterion (real vs graffiti)
+feature_extractor = FeatureExtractor().cuda()
+label_predictor = LabelPredictor().cuda()
+domain_classifier = DomainClassifier().cuda()
+
+class_criterion = nn.CrossEntropyLoss()
+domain_criterion = nn.BCEWithLogitsLoss()
+
+optimizer_F = optim.Adam(feature_extractor.parameters())
+optimizer_C = optim.Adam(label_predictor.parameters())
+optimizer_D = optim.Adam(domain_classifier.parameters())
+
+#%%
+def train_epoch(source_dataloader, target_dataloader, lamb):
+    '''
+      Args:
+        source_dataloader: source data的dataloader
+        target_dataloader: target data的dataloader
+        lamb: 調控adversarial的loss係數。
+    '''
+
+    # D loss: Domain Classifier的loss
+    # F loss: Feature Extrator & Label Predictor的loss
+    # total_hit: 計算目前對了幾筆 total_num: 目前經過了幾筆
+    running_D_loss, running_F_loss = 0.0, 0.0
+    total_hit, total_num = 0.0, 0.0
+
+    for i, ((source_data, source_label), (target_data, _)) in enumerate(zip(source_dataloader, target_dataloader)):
+
+        source_data = source_data.cuda()
+        source_label = source_label.cuda()
+        target_data = target_data.cuda()
+        
+        # 我們把source data和target data混在一起，否則batch_norm可能會算錯 (兩邊的data的mean/var不太一樣)
+        mixed_data = torch.cat([source_data, target_data], dim=0)
+        domain_label = torch.zeros([source_data.shape[0] + target_data.shape[0], 1]).cuda()
+        # 設定source data的label為1
+        domain_label[:source_data.shape[0]] = 1
+
+        # Step 1 : 訓練Domain Classifier
+        feature = feature_extractor(mixed_data)
+        # 因為我們在Step 1不需要訓練Feature Extractor，所以把feature detach避免loss backprop上去。
+        domain_logits = domain_classifier(feature.detach())
+        loss = domain_criterion(domain_logits, domain_label)
+        running_D_loss+= loss.item()
+        loss.backward()
+        optimizer_D.step()
+
+        # Step 2 : 訓練Feature Extractor和Domain Classifier
+        class_logits = label_predictor(feature[:source_data.shape[0]])
+        domain_logits = domain_classifier(feature)
+        # loss為原本的class CE - lamb * domain BCE，相減的原因同GAN中的Discriminator中的G loss。
+        loss = class_criterion(class_logits, source_label) - lamb * domain_criterion(domain_logits, domain_label)
+        running_F_loss+= loss.item()
+        loss.backward()
+        optimizer_F.step()
+        optimizer_C.step()
+
+        optimizer_D.zero_grad()
+        optimizer_F.zero_grad()
+        optimizer_C.zero_grad()
+
+        total_hit += torch.sum(torch.argmax(class_logits, dim=1) == source_label).item()
+        total_num += source_data.shape[0]
+
+    return running_D_loss / (i+1), running_F_loss / (i+1), total_hit / total_num
+
+# 訓練200 epochs
+for epoch in range(500):
+    train_D_loss, train_F_loss, train_acc = train_epoch(source_dataloader, target_dataloader, lamb=0.1)
+
+    torch.save(feature_extractor.state_dict(), f'extractor_model.bin')
+    torch.save(label_predictor.state_dict(), f'predictor_model.bin')
+
+    print('epoch {:>3d}: train D loss: {:6.4f}, train F loss: {:6.4f}, acc {:6.4f}'.format(epoch, train_D_loss, train_F_loss, train_acc))
+
+
+# %%
+# inference
+result = []
+label_predictor.eval()
+feature_extractor.eval()
+for i, (test_data, _) in enumerate(test_dataloader):
+    test_data = test_data.cuda()
+
+    class_logits = label_predictor(feature_extractor(test_data))
+
+    x = torch.argmax(class_logits, dim=1).cpu().detach().numpy()
+    result.append(x)
+
+import pandas as pd
+result = np.concatenate(result)
+
+# Generate your submission
+df = pd.DataFrame({'id': np.arange(0,len(result)), 'label': result})
+df.to_csv('DaNN_submission.csv',index=False)
+
+# %%
